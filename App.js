b20c28bdc14f44b456db0531cc0dff9be2996681
Servio Mora <servio.mora19@gmail.com>
2017-07-08 11:55:13 -0400

    slight fixes

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is my iOS Maps Project</Text>
+        <Text>This is my iOS Maps App</Text>
       </View>
     );
   }
